d1b75b70b7d9b41dae61e90fac036a7ddd1aedfd
jdidat <jdidat@purdue.edu>
2018-01-21 18:14:57 -0500

    finished required user stories

---
+++ Flix/Views/PosterCell.swift
@@ -0,0 +1,14 @@
+//
+//  PosterCell.swift
+//  Flix
+//
+//  Created by Jackson Didat on 1/20/18.
+//  Copyright © 2018 jdidat. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    @IBOutlet weak var posterImageView: UIImageView!
+    
+}
